b89f9c81ceefbfe75222fd87067a1a69697ac51d
Sarbeng <sarbengbaafi@gmail.com>
2022-12-01 11:48:16 -0800

    edited 0-add.py to cater for the __name__

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,17 +1,9 @@
 #!/usr/bin/python3
-"""
-Program that 
-imports def add(a, b): from the file add_0.py
-print resulrs of addition 1+2 = 3
-Args:
-    a: 1
-    b: 2
-print: <a value> + <b value> = <add(a, b) value>
-followed with a new line
-"""
-from add_0 import add
 
-a = 1
-b = 2
+if __name__ == "__main__":
+    """Print the sum of 1 and 2."""
+    from add_0 import add
 
-print("{} + {} = {}".format(a, b, add(a, b)))
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
