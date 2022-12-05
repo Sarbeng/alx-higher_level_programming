b447bdb66f2e5d5218c62e4d34d51840d022630a
Sarbeng <sarbengbaafi@gmail.com>
2022-12-05 03:00:39 -0800

    Print a list of integers

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+function that prints all integers of a list.
+
+
+"""
+def print_list_integer(my_list=[]):
+    for i in my_list:
+        print(f"{i}")
+
+# squares = [x**2 for x in range(10)]
+# print_list_integer(squares)
