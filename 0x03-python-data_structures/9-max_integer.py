c96b404b18336ec3124347f74ca3c76cb434eb9b
Sarbeng <sarbengbaafi@gmail.com>
2022-12-05 14:02:38 -0800

    i tried to use sort but got zero so i used a for loop to find the man value in the list

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,6 +1,14 @@
+#!/usr/bin/python3
+
+
 def max_integer(my_list=[]):
-    if my_list == []:
-        return None
-    else:
-        my_list.sort()
-        return(my_list[-1])
+    """Find the biggest integer of a list."""
+    if len(my_list) == 0:
+        return (None)
+
+    big = my_list[0]
+    for i in range(len(my_list)):
+        if my_list[i] > big:
+            big = my_list[i]
+
+    return (big)
