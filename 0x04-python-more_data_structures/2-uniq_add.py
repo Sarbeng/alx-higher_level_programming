88cef9aef6711667e5300b7fe1f7df7e1eed1af3
Sarbeng <sarbengbaafi@gmail.com>
2022-12-07 09:29:43 -0800

    pycode edit for 2-uniq_add.py

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 
 def uniq_add(my_list=[]):
-    #first we need to get the unique values
+    # first we need to get the unique values
     uniq = list(set(my_list))
-    #set an empty variable to do the addition
+    # set an empty variable to do the addition
     add = 0
-    #loop through our unique values and add them
+    # loop through our unique values and add them
     for i in range(len(uniq)):
         add += uniq[i]
     return add
-
